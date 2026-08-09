5143226eaaa46a687584d11753da7cac87bff8a4
Bruce Zhang <zttt183525594@gmail.com>
2026-08-09 10:38:26 +0800

    feat: expose iOS video transport controls

--- shared/src/iosMain/kotlin/org/feeluown/mobile/IosVideoOutput.kt
+++ shared/src/iosMain/kotlin/org/feeluown/mobile/IosVideoOutput.kt
@@ -4,6 +4,7 @@ import platform.UIKit.UIViewController
 
 interface IosVideoOutput {
     fun makePlayer(url: String, videoUrl: String, audioUrl: String, headers: Map<String, String>): UIViewController
+
     fun updatePlayer(
         viewController: UIViewController,
         url: String,
@@ -11,5 +12,26 @@ interface IosVideoOutput {
         audioUrl: String,
         headers: Map<String, String>,
     )
+
     fun releasePlayer(viewController: UIViewController)
+
+    fun play(viewController: UIViewController)
+
+    fun pause(viewController: UIViewController)
+
+    fun seekTo(viewController: UIViewController, positionMs: Long)
+
+    fun positionMs(viewController: UIViewController): Long
+
+    fun durationMs(viewController: UIViewController): Long
+
+    fun bufferedMs(viewController: UIViewController): Long
+
+    fun isPlaying(viewController: UIViewController): Boolean
+
+    fun videoWidth(viewController: UIViewController): Int
+
+    fun videoHeight(viewController: UIViewController): Int
+
+    fun setFullscreenOrientation(isFullscreen: Boolean, landscape: Boolean)
 }
