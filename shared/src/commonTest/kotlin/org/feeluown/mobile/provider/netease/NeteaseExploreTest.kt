6527e3726b2023b3903513d284fc74378d44b97d
Bruce Zhang <zttt183525594@gmail.com>
2026-08-09 14:19:41 +0800

    test NetEase explore feature ids

---
+++ shared/src/commonTest/kotlin/org/feeluown/mobile/provider/netease/NeteaseExploreTest.kt
@@ -0,0 +1,33 @@
+package org.feeluown.mobile.provider.netease
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class NeteaseExploreTest {
+    @Test
+    fun featureRequestParsesDerivedFilterParameters() {
+        val id = neteaseFeatureId(
+            NETEASE_PLAYLIST_SQUARE,
+            "cat" to "R&B/Soul",
+            "order" to "new",
+        )
+
+        val request = parseNeteaseFeatureRequest(id)
+
+        assertEquals(NETEASE_PLAYLIST_SQUARE, request.baseId)
+        assertEquals("R&B/Soul", request.params["cat"])
+        assertEquals("new", request.params["order"])
+    }
+
+    @Test
+    fun presentationMetadataIsIgnoredWhenParsingRequest() {
+        val request = parseNeteaseFeatureRequest(
+            "netease_artist_square|area=96|type=3|initial=77^filters^presentation-only",
+        )
+
+        assertEquals(NETEASE_ARTIST_SQUARE, request.baseId)
+        assertEquals("96", request.params["area"])
+        assertEquals("3", request.params["type"])
+        assertEquals("77", request.params["initial"])
+    }
+}
