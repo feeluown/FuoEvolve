ada3f19705b5763b6ef081fd372d802376d851f3
Bruce Zhang <zttt183525594@gmail.com>
2026-08-17 00:14:57 +0800

    fix: match predictive back backdrop to dark theme

--- shared/src/iosMain/kotlin/org/feeluown/mobile/PlatformTheme.ios.kt
+++ shared/src/iosMain/kotlin/org/feeluown/mobile/PlatformTheme.ios.kt
@@ -2,6 +2,10 @@ package org.feeluown.mobile
 
 import androidx.compose.material3.ColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable
 internal actual fun platformDynamicColorScheme(darkTheme: Boolean): ColorScheme? = null
+
+@Composable
+internal actual fun platformWindowSurfaceEffect(surfaceColor: Color) = Unit
