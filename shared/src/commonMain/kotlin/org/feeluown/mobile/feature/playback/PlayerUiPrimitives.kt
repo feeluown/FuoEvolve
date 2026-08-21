84e514bd4e15e7ccbde29a53f37c1522ac3b895a
Bruce Zhang <zttt183525594@gmail.com>
2026-08-21 17:05:49 +0800

    fix: restore player transition import

--- shared/src/commonMain/kotlin/org/feeluown/mobile/feature/playback/PlayerUiPrimitives.kt
+++ shared/src/commonMain/kotlin/org/feeluown/mobile/feature/playback/PlayerUiPrimitives.kt
@@ -10,6 +10,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
