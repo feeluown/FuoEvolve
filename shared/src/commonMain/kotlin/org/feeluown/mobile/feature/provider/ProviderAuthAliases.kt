6fd9fcf07126406e19ebeb5976ab518e5adb83b4
Bruce Zhang <zttt183525594@gmail.com>
2026-08-21 10:12:53 +0800

    refactor: keep platform auth calls owner driven

---
+++ shared/src/commonMain/kotlin/org/feeluown/mobile/feature/provider/ProviderAuthAliases.kt
@@ -0,0 +1,7 @@
+package org.feeluown.mobile
+
+fun ProviderAuthFeatureController.loginWithYtmusicHeaderFile(json: String) =
+    loginYtmusicWithHeaderFile(json)
+
+fun ProviderAuthFeatureController.loginWithYtmusicOAuthJson(json: String) =
+    loginYtmusicWithOAuthJson(json)
