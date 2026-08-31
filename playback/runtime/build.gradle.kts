1841bd51b9442a5287f7e354d0414b4e96987f6c
Bruce Zhang <zttt183525594@gmail.com>
2026-08-31 23:52:05 +0800

    build: migrate playback runtime to Android KMP plugin

--- playback/runtime/build.gradle.kts
+++ playback/runtime/build.gradle.kts
@@ -1,10 +1,15 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
 }
 
 kotlin {
-    androidTarget {
+    android {
+        namespace = "org.feeluown.mobile.playback.runtime"
+        compileSdk = libs.versions.androidCompileSdk.get().toInt()
+        minSdk = libs.versions.androidMinSdk.get().toInt()
+        withHostTest {}
+
         compilerOptions {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
@@ -21,19 +26,5 @@ kotlin {
         commonTest.dependencies {
             implementation(kotlin("test"))
         }
-    }
-}
-
-android {
-    namespace = "org.feeluown.mobile.playback.runtime"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 24
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
     }
 }
