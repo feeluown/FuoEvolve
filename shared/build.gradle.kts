78bf72a69a24970a5b65780b3e23142c5fc572b3
Bruce Zhang <zttt183525594@gmail.com>
2026-09-01 00:00:33 +0800

    build: migrate shared module to Android KMP plugin

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,16 +1,25 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlinx.kover)
 }
 
 kotlin {
-    androidTarget {
+    android {
+        namespace = "org.feeluown.mobile.shared"
+        compileSdk = libs.versions.androidCompileSdk.get().toInt()
+        minSdk = libs.versions.androidMinSdk.get().toInt()
+        withHostTest {}
+
         compilerOptions {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+
+        lint {
+            disable.add("NullSafeMutableLiveData")
         }
     }
 
@@ -86,24 +95,6 @@ kotlin {
             implementation(libs.androidx.datastore.preferences)
             implementation(libs.ktor.client.darwin)
         }
-    }
-}
-
-android {
-    namespace = "org.feeluown.mobile.shared"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 24
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    lint {
-        disable.add("NullSafeMutableLiveData")
     }
 }
 
