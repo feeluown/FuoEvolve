7df7ed905295d413dd8616bf0aa4deb6ebde9dd7
Bruce Zhang <zttt183525594@gmail.com>
2026-09-02 08:29:39 +0800

    test: cover desktop tray policy

---
+++ desktopApp/src/test/kotlin/org/feeluown/mobile/desktop/DesktopTrayControllerTest.kt
@@ -0,0 +1,21 @@
+package org.feeluown.mobile.desktop
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class DesktopTrayControllerTest {
+    @Test
+    fun selectsNativeTrayBackendPerDesktopPlatform() {
+        assertEquals(DesktopTrayBackend.WindowsAwt, desktopTrayBackend("Windows 11"))
+        assertEquals(DesktopTrayBackend.MacAwt, desktopTrayBackend("Mac OS X"))
+        assertEquals(DesktopTrayBackend.MacAwt, desktopTrayBackend("Darwin"))
+        assertEquals(DesktopTrayBackend.LinuxStatusNotifier, desktopTrayBackend("Linux"))
+        assertEquals(DesktopTrayBackend.Unsupported, desktopTrayBackend("FreeBSD"))
+    }
+
+    @Test
+    fun hidesOnlyWhenTrayCanRestoreWindow() {
+        assertEquals(DesktopCloseBehavior.HideToTray, desktopCloseBehavior(trayAvailable = true))
+        assertEquals(DesktopCloseBehavior.KeepVisible, desktopCloseBehavior(trayAvailable = false))
+    }
+}
