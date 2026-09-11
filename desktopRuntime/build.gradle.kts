cf933e32f48dde84151c3a1496ee58e95de7b0c1
Bruce Zhang <zttt183525594@gmail.com>
2026-09-11 09:54:51 +0800

    build(desktop): add native runtime service dependencies

--- desktopRuntime/build.gradle.kts
+++ desktopRuntime/build.gradle.kts
@@ -8,11 +8,14 @@ kotlin {
 
 dependencies {
     implementation(project(":shared"))
+    implementation(project(":persistence:listening"))
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.jna)
     implementation(libs.jna.platform)
     implementation(libs.credential.secure.storage)
+    implementation(libs.jaudiotagger)
+    implementation("io.github.vinceglb:filekit-dialogs:0.15.0")
 
     testImplementation(kotlin("test"))
 }
