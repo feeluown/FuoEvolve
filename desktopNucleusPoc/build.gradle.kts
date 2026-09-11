1bdfc50690d033f2278b62ba827a8818ccfb46fd
Bruce Zhang <zttt183525594@gmail.com>
2026-09-12 00:03:01 +0800

    fix: restore Native Image compatibility march

--- desktopNucleusPoc/build.gradle.kts
+++ desktopNucleusPoc/build.gradle.kts
@@ -1,3 +1,4 @@
+import dev.nucleusframework.desktop.application.dsl.NativeImageMarch
 import dev.nucleusframework.desktop.application.dsl.TargetFormat
 import java.io.File
 import java.util.zip.ZipFile
@@ -434,6 +435,7 @@ nucleus.application {
     graalvm {
         isEnabled.set(true)
         imageName.set("fuoevolve")
+        march.set(NativeImageMarch.COMPATIBILITY)
     }
 }
 
