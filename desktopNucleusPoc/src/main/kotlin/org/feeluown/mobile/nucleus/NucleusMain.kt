8d3021ede201b65ee0bafa2430bf346fcb8609f2
Bruce Zhang <zttt183525594@gmail.com>
2026-09-12 02:02:59 +0800

    feat(desktop): install Tao GPU video surface

--- desktopNucleusPoc/src/main/kotlin/org/feeluown/mobile/nucleus/NucleusMain.kt
+++ desktopNucleusPoc/src/main/kotlin/org/feeluown/mobile/nucleus/NucleusMain.kt
@@ -63,6 +63,7 @@ import org.feeluown.mobile.installDesktopAppLogger
 import org.feeluown.mobile.installDesktopJniMpvVideoControllerFactory
 import org.feeluown.mobile.installDesktopListeningHistorySinkFactory
 import org.feeluown.mobile.installDesktopLocalMusicRepositoryFactory
+import org.feeluown.mobile.installDesktopPlatformVideoSurface
 import org.feeluown.mobile.installDesktopPlaybackEngineFactory
 import org.feeluown.mobile.installDesktopPlaybackSessionIntegrationFactory
 import org.feeluown.mobile.installDesktopProviderCredentialStoreFactory
@@ -85,6 +86,7 @@ fun main(args: Array<String>) {
     installDesktopListeningHistorySinkFactory(::createDesktopRuntimeListeningHistorySink)
     installDesktopLocalMusicRepositoryFactory(::createDesktopRuntimeLocalMusicRepository)
     installDesktopJniMpvVideoControllerFactory()
+    installDesktopPlatformVideoSurface(NucleusMpvVideoSurface)
     installDesktopTextFileDialogProviderFactory {
         createDesktopNativeTextFileDialogProvider(requireNativeLinuxPortal = true)
     }
